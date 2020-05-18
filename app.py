54cf809c5f6c2ed11d6936bbb02bd7d10599b445
jonathanstanislaus05 <jonathanstanislaus05@gmail.com>
2020-05-18 11:08:48 +0700

    add new line

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 def index():
     headline = 'Hello World!'
     return render_template("index.html", headline=headline)
+
+@app.route('/login')
+def login():
+    headline = 'Successfully login!'
+    return render_template("index.html", headline=headline)
+
+@app.route('/home')
+def home():
+    headline = 'Coming soon!'
+    return render_template("home.html", headline=headline)
